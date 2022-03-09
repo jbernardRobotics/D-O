2930b10c4bd3ce5c929ad8983ca7229e4e3c0862
jacob <jbernard@wpi.edu>
2022-03-09 11:11:41 -0500

    Started the Body class

---
+++ D-O Main Body/src/Body.h
@@ -0,0 +1,14 @@
+#ifndef BODY_H
+#define BODY_H
+
+#include <Arduino.h>
+
+class Body
+{
+private:
+public:
+    Body(void);
+    void bodySetup();
+};
+
+#endif
--- D-O Main Body/src/body.cpp
+++ D-O Main Body/src/body.cpp
@@ -1,2 +1,9 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <body.h>
 
+Body::Body(void) {}
+
+void Body::bodySetup()
+{
+}
